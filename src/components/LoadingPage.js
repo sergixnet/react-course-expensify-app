beb7c37a0eaa6aba8c1b77b9e2ac22b9efd85213
Sergio Peña <sergiop79@gmail.com>
2020-07-24 12:32:52 +0200

    Add expensify styles

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loadingpage = () => (
+  <div className="loader">
+    <img className="loader__image" src="/images/loader.gif" alt=""/>
+  </div>
+);
+
+export default Loadingpage;
